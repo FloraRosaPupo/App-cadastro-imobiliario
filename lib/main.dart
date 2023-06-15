fa9245e58e05cb0035d2e32d71470f02d1dc58ac
FloraRosaPupo <florapupo@gmail.com>
2023-06-15 12:09:55 -0300

    instalacao firebase

--- lib/main.dart
+++ lib/main.dart
@@ -30,8 +30,12 @@ import 'package:flutter_launcher_icons/xml_templates.dart';
 import 'package:date_format/date_format.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:firebase_core/firebase_core.dart';
+//import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  /*await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );*/
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     localizationsDelegates: [
@@ -49,7 +53,5 @@ void main() {
       errorColor: Colors.red,
       hoverColor: Colors.redAccent,
     ),
-    
-  )
-  );
+  ));
 }
