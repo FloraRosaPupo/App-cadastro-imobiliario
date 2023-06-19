ed46cd2f2ba4d66af63a790a8e180f7e7189bc6c
FloraRosaPupo <florapupo@gmail.com>
2023-06-19 11:36:47 -0300

    alteracoes no login e cadastro

--- lib/pages/registerpage.dart
+++ lib/pages/registerpage.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: unused_import
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:projeto_prefeitura/main.dart';
