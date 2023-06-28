adbb9063f01b3777b7db2c480662b411c1a421fc
FloraRosaPupo <florapupo@gmail.com>
2023-06-28 15:46:41 -0300

    galeria quarteiroes

--- lib/pages/galeria.dart
+++ lib/pages/galeria.dart
@@ -1,4 +1,11 @@
+// ignore_for_file: unused_import
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import '../functions.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:path/path.dart' as path;
 
 class Galeria extends StatefulWidget {
   const Galeria({super.key});
@@ -8,18 +15,81 @@ class Galeria extends StatefulWidget {
 }
 
 class _GaleriaState extends State<Galeria> {
-  //List<String> ids = [];
+  final _firebaseAuth = FirebaseAuth.instance;
+  String nome = '';
+  String email = '';
+
+  //
+  @override
+  initState() {
+    chamarUsuario();
+  }
+
+  chamarUsuario() async {
+    User? usuario = await _firebaseAuth.currentUser;
+    if (usuario != null) {
+      print(usuario);
+      setState(() {
+        nome = usuario.displayName!;
+        email = usuario.email!;
+      });
+    }
+  }
+
+  Future getGridView() async {
+    var firestore = FirebaseFirestore.instance;
+    QuerySnapshot snap =
+        await firestore.collection("gridData").get() /*Pode dar errado*/;
+
+    //pode ta errado
+    return snap.docs;
+  }
+
+  Future<Null> getRegresh() async {
+    await Future.delayed(Duration(seconds: 3));
+    setState(() {
+      getGridView();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      gridDelegate:
-          SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-      itemBuilder: (context, index) {
-        return Image.network(
-            'https://drive.google.com/file/d/1K-Yzvcl0NGmQuTzPQOEyIyucIUrCqO_Z/view?usp=sharing');
-      },
-      //itemCount: ids.length,
+    return Scaffold(
+      appBar: appBarDinamica(),
+      drawer: menuLateralDinamico(nome, email),
+      body: FutureBuilder(
+        future: getGridView(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            return RefreshIndicator(
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3),
+                itemCount: snapshot.data.length,
+                itemBuilder: (context, index) {
+                  var ourData = snapshot.data[index];
+
+                  return Card(
+                    elevation: 10.0,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0)),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child:
+                          Image.network(ourData.data["img"], fit: BoxFit.cover),
+                    ),
+                  );
+                },
+              ),
+              onRefresh: getRegresh,
+            );
+          }
+        },
+      ),
     );
   }
 }
