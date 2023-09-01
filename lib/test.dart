d0f640886edcbba10b31134c3f1aa6506b8694f1
FloraRosaPupo <florapupo@gmail.com>
2023-09-01 09:29:24 -0300

    teste listagem de dados

--- lib/test.dart
+++ lib/test.dart
@@ -65,7 +65,8 @@ class _ListaImoveisState extends State<ListaImoveis> {
                       onTap: () {
                         Navigator.of(context).push(MaterialPageRoute(
                           builder: (context) => ImovelDetalhes(
-                              imovel: imoveis[index], onUpdate: atualizarImovel),
+                              imovel: imoveis[index],
+                              onUpdate: atualizarImovel),
                         ));
                       },
                       leading: Image.network(
@@ -111,6 +112,123 @@ class _ListaImoveisState extends State<ListaImoveis> {
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class ImovelDetalhes extends StatelessWidget {
+  final Map<String, dynamic> imovel;
+  final Function(Map<String, dynamic>) onUpdate;
+
+  ImovelDetalhes({required this.imovel, required this.onUpdate});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBarDinamica(),
+      body: ConstrainedBox(
+        constraints: BoxConstraints(
+          maxHeight: MediaQuery.of(context).size.height - kToolbarHeight,
+        ),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Endereço: ${imovel['street']}',
+                  style: TextStyle(fontSize: 20),
+                ),
+                Text(
+                  'Número: ${imovel['number']}',
+                  style: TextStyle(fontSize: 20),
+                ),
+                Text(
+                  'Proprietário: ${imovel['owner']}',
+                  style: TextStyle(fontSize: 20),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () async {
+                    await Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) =>
+                          EditarImovel(imovel: imovel, onUpdate: onUpdate),
+                    ));
+                    Navigator.of(context).popUntil(
+                        (route) => route.isFirst); // Volta para a tela inicial.
+                  },
+                  child: Text('Editar Imóvel'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class EditarImovel extends StatefulWidget {
+  final Map<String, dynamic> imovel;
+  final Function(Map<String, dynamic>) onUpdate;
+
+  EditarImovel({required this.imovel, required this.onUpdate});
+
+  @override
+  _EditarImovelState createState() => _EditarImovelState();
+}
+
+class _EditarImovelState extends State<EditarImovel> {
+  TextEditingController streetController = TextEditingController();
+  TextEditingController numberController = TextEditingController();
+  TextEditingController ownerController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    streetController.text = widget.imovel['street'];
+    numberController.text = widget.imovel['number'];
+    ownerController.text = widget.imovel['owner'];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBarDinamica(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextFormField(
+              controller: streetController,
+              decoration: InputDecoration(labelText: 'Endereço'),
+            ),
+            TextFormField(
+              controller: numberController,
+              decoration: InputDecoration(labelText: 'Número'),
+            ),
+            TextFormField(
+              controller: ownerController,
+              decoration: InputDecoration(labelText: 'Proprietário'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                widget.imovel['street'] = streetController.text;
+                widget.imovel['number'] = numberController.text;
+                widget.imovel['owner'] = ownerController.text;
+
+                widget.onUpdate(widget.imovel);
+
+                Navigator.of(context).pop(); // Volta para a tela de detalhes.
+              },
+              child: Text('Salvar Alterações'),
+            ),
+          ],
+        ),
       ),
     );
   }
